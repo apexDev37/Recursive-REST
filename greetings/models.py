8bbc896c01732f386ec9936621b4eeaa975e4da3
apexdev <mwangi.em37@gmail.com>
2023-07-01 14:46:39 +0300

    feat: Add model
    Add the initial 'greeting' model and include Meta class to order items by date-created.
    Add docstring for the module and model class.
    Modify timezone setting to my current local timezone.

--- greetings/models.py
+++ greetings/models.py
@@ -1,3 +1,25 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+"""
+  Module Name:      greetings/models.py
+  Module Purpose:   contains the Django models for the greetings application.
+  Author:           apexDev37 (Eugene M.)
+  Version:          1.0
+  Last Modified:    01/07
+  Dependencies:     uuid, models
+"""
+
+
+class Greeting(models.Model):
+    """
+    Represents a custom greeting from a user.
+    """
+
+    greeting_id = models.UUIDField(primary_key=True, default=uuid.uuid5, editable=False)
+    greeting_text = models.CharField(unique=True, blank=False, max_length=50)
+    greeting_created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        ordering = ["greeting_created_at"]
