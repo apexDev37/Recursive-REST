7546f9591ee1c7f210b0c108f6f927ee846c800d
apexdev <mwangi.em37@gmail.com>
2023-07-08 10:27:14 +0300

    test: Check param integrity
    Add two tests to verify validation logic for the query param 'greeting'.
    Test that app logic error handles the following cases:
      1. blank/empty value passed to the query param
      2. value passed to the query param contains unwanted (non-alphabetic) chars

--- greetings/tests/test_views.py
+++ greetings/tests/test_views.py
@@ -108,9 +108,39 @@ class GreetingsViewLogicTestCase(TestCase):
     """ Given a present query_param key in the url, on a post, don't raise an exception. """
 
     # Given
-    request_with_param_key = self.factory.post(GREETING_ENDPOINT + GREETING_PARAM)
+    request_with_param_key = self.factory.post(GREETING_URI)
 
     try:
       save_custom_greeting(request_with_param_key)
-    except:
-      self.failIf(ValueError, msg="Exception should NOT be raised, query param key is present.")
+    except Exception:
+      self.failIf( Exception is ValueError)
+
+  def test_should_raise_exception_when_query_param_value_is_blank_or_none(self) -> None:
+    """ Given a blank or none query_param value, raise an exception. """
+
+    # Given
+    blank_param_value = ''
+    
+    # When
+    request = self.factory.post(GREETING_URI + blank_param_value)
+    
+    # Then
+    with self.assertRaisesMessage(
+      ValueError, 
+      "Value for query param: greeting cannot be blank or empty."):
+        save_custom_greeting(request)
+  
+  def test_should_raise_exception_if_query_param_value_has_non_alphabetical_chars(self) -> None:
+    """ Given a value that contains non alphabetical chars, raise an exception. """
+
+    # Given
+    value = '_greeting@37'
+    
+    # When
+    request = self.factory.post(GREETING_URI + value)
+    
+    self.assertIsInstance(value, str)
+    with self.assertRaisesMessage(
+      ValueError, 
+      "Value for query param: greeting cannot contain non alphabetic chars."):
+        save_custom_greeting(request)
