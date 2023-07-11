4a6dae29209ad46e9470f19a93673f8b91b7f67e
apexdev <mwangi.em37@gmail.com>
2023-07-11 21:45:17 +0300

    feat: Add recursive logic
    Add recursive logic. Create 'clone' request with RequestFactory.
    Update path for request used in recursive call with new query_param value (goodbye).
    Return a Response instance to facilitate tests and render semantic response data for clients.
    Add temporary logs to debug info to the console to aid in test results.

--- greetings/views.py
+++ greetings/views.py
@@ -5,11 +5,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 
+from django.test import RequestFactory
+
 from greetings.models import Greeting
 from greetings.serializers import GreetingSerializer
 from greetings.utils.validators import * 
 
 logger = logging.getLogger(__name__)
+factory = RequestFactory()
 
 @api_view(["GET"])
 def list_greetings(request: Request) -> Response:
@@ -27,17 +30,37 @@ def save_custom_greeting(request: Request) -> Response:
   """Save a custom greeting from a user."""
 
   custom_greeting = validate_query_param(request)
+  logger.debug(f'user passed query_param value: {custom_greeting}')
+  if custom_greeting == 'Kwaheri':
+    logger.debug(f'FINAL response')
+    return Response(
+      {
+        "status_code": status.HTTP_201_CREATED,
+        "message": "Success",
+        "description": "Saved custom greeting from user.",
+        "greeting": custom_greeting,
+        "goodbye": request.query_params['greeting'],
+      }, 
+      status=status.HTTP_201_CREATED)    
 
   try:    
     greeting = Greeting(greeting_text=custom_greeting)
     greeting.save()
-    logger.info(f'save custom greeting "{greeting.greeting_text}" from user.')
+    logger.info(f'Save custom greeting "{greeting.greeting_text}" from user.')
+        
+    # Logic for making a cloned request with different param
+    custom_goodbye = 'Kwaheri'
+    path: str = request.build_absolute_uri()
+    logger.debug(f'Path for initial request: {path}.')
+    path = path.replace(f'={custom_greeting}', f'={custom_goodbye}', 1)
+    recursive_request = factory.post(path=path)
     
-    logger.debug('recursive call to the view.save_custom_greeting.')
-    return save_custom_greeting(request)
+    logger.debug(f'Path for recursive request: {path}.')
+    logger.debug(f'Initial request query_param value: {request.query_params["greeting"]}.')
+    logger.debug(f'Recursive request query_param value: {recursive_request.META["QUERY_STRING"]}.')
     
-    return Response(
-      {"message": "Greeting saved"}, status=status.HTTP_201_CREATED)
+    logger.debug('recursive call to api_view: views.save_custom_greeting.')
+    return save_custom_greeting(recursive_request)
 
   except Exception:
     return Response(
