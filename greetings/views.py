7be257867965fc444c62d1068d9fb6396512a07e
apexdev <mwangi.em37@gmail.com>
2023-07-11 22:41:42 +0300

    refactor: Clean up logic
    Extract functions to encapsulate view logic in smaller units.
    Declutter view by cleaning up logs used for debugging and testing purposes.
    (chore) Ensure tests pass after refactor.

--- greetings/views.py
+++ greetings/views.py
@@ -5,11 +5,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 
+from django.core.handlers.wsgi import WSGIRequest
 from django.test import RequestFactory
 
 from greetings.models import Greeting
 from greetings.serializers import GreetingSerializer
 from greetings.utils.validators import * 
+
+CUSTOM_GOODBYE: str = 'Kwaheri'
 
 logger = logging.getLogger(__name__)
 factory = RequestFactory()
@@ -30,35 +33,15 @@ def save_custom_greeting(request: Request) -> Response:
   """Save a custom greeting from a user."""
 
   custom_greeting = validate_query_param(request)
-  logger.debug(f'user passed query_param value: {custom_greeting}')
-  if custom_greeting == 'Kwaheri':
-    logger.debug(f'FINAL response')
-    return Response(
-      {
-        "status_code": status.HTTP_201_CREATED,
-        "message": "Success",
-        "description": "Saved custom greeting from user.",
-        "greeting": custom_greeting,
-        "goodbye": request.query_params['greeting'],
-      }, 
-      status=status.HTTP_201_CREATED)    
-
+  if custom_greeting == CUSTOM_GOODBYE:
+    return custom_greeting_response(custom_greeting, request)
+  
   try:    
     greeting = Greeting(greeting_text=custom_greeting)
     greeting.save()
     logger.info(f'Save custom greeting "{greeting.greeting_text}" from user.')
         
-    # Logic for making a cloned request with different param
-    custom_goodbye = 'Kwaheri'
-    path: str = request.build_absolute_uri()
-    logger.debug(f'Path for initial request: {path}.')
-    path = path.replace(f'={custom_greeting}', f'={custom_goodbye}', 1)
-    recursive_request = factory.post(path=path)
-    
-    logger.debug(f'Path for recursive request: {path}.')
-    logger.debug(f'Initial request query_param value: {request.query_params["greeting"]}.')
-    logger.debug(f'Recursive request query_param value: {recursive_request.META["QUERY_STRING"]}.')
-    
+    recursive_request = update_request_query_param(custom_greeting, request)
     logger.debug('recursive call to api_view: views.save_custom_greeting.')
     return save_custom_greeting(recursive_request)
 
@@ -66,10 +49,26 @@ def save_custom_greeting(request: Request) -> Response:
     return Response(
       {"error": "Invalid greeting"}, status=status.HTTP_400_BAD_REQUEST)  
 
+
+def custom_greeting_response(custom_greeting, request) -> Response:
+  return Response({
+    "status_code": status.HTTP_201_CREATED,
+    "message": "Success",
+    "description": "Saved custom greeting from user.",
+    "greeting": custom_greeting,
+    "goodbye": request.query_params['greeting'],
+  }, status=status.HTTP_201_CREATED)    
+
+
 def validate_query_param(request: Request) -> str:
   GreetingPathValidator.validate_param_key_present(request)
   custom_greeting = request.query_params['greeting']
   GreetingParamValidator.validate_not_blank(custom_greeting)
   GreetingValueValidator.validate_param_value(custom_greeting)
   return custom_greeting
-  
+
+
+def update_request_query_param(initial_param: str, initial_request: Request) -> WSGIRequest:
+  path: str = initial_request.build_absolute_uri()
+  updated_path = path.replace(f'={initial_param}', f'={CUSTOM_GOODBYE}', 1)
+  return factory.post(path=updated_path)
