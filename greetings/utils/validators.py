aa677a644730e2d55be82182e848a05efac04b4e
apexdev <mwangi.em37@gmail.com>
2023-07-13 11:54:54 +0300

    refactor: Clean up validators
    Remove redundant validator 'GreetingParamValidator'.
    Depend on Django model validation to validate field 'not blank'.
    Add docstrings to provide basic docs for class validators.
    Rename 'GreetingParamValidator' to be more semantic and expressive.

--- greetings/utils/validators.py
+++ greetings/utils/validators.py
@@ -6,18 +6,23 @@ from rest_framework.exceptions import ValidationError
 from greetings.utils.constants import GreetingsPathConstants as path 
 
 class GreetingPathValidator:
+  """
+  Validator class to ensure that a required param key
+  is present in the request path.
+  """
+  
   @staticmethod
   def validate_param_key_present(request: Request) -> None:
     url_path = request.build_absolute_uri()
     if str(path.GREETING_PARAM_KEY) not in url_path:
       raise ValueError("Key for required query param: greeting is missing.")
 
-class GreetingParamValidator:  
-  def __call__(self, value: str) -> None:
-    if value.strip() == '':
-      raise ValidationError("Value for query param: greeting cannot be blank or empty.")
-
-class GreetingValueValidator:
+class AlphaCharsValidator:
+  """
+  Callable validator class for the greeting model.
+  Validate that a models.CharField() only contains alphabetical chars. 
+  """
+  
   def __call__(self, value: str) -> None:
     pattern = r'^[a-zA-Z]+$'
     if not re.match(pattern, value):
