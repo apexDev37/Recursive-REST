70754ad884921b181baaab06f6928a5254c886b7
apexdev <mwangi.em37@gmail.com>
2023-07-13 11:19:33 +0300

    refactor: Migrate validators to model
    Migrate validators from the views to models file.
    Make validator classes callable to be passed to field validators list.
    Ensure validation runs on 'save' for model instance.
    (chore) Ensure tests pass after refactor.

--- greetings/models.py
+++ greetings/models.py
@@ -3,6 +3,11 @@ import uuid
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from greetings.utils.validators import (
+  GreetingParamValidator,
+  GreetingValueValidator,
+) 
+
 """
   Module Name:      greetings/models.py
   Module Purpose:   contains the Django models for the greetings application.
@@ -19,14 +24,16 @@ class Greeting(models.Model):
     """
 
     greeting_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    greeting_text = models.CharField(unique=True, blank=False, max_length=50)
+    greeting_text = models.CharField(
+      unique=True, 
+      blank=False, 
+      max_length=50,
+      validators=[GreetingParamValidator(), GreetingValueValidator()])
     greeting_created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     class Meta:
         ordering = ["greeting_created_at"]
 
     def save(self, *args, **kwargs):
-        if not self.greeting_text.strip():
-            raise ValidationError("greeting_text field cannot be null or blank.")
-
+        self.full_clean()
         super().save(*args, **kwargs)
