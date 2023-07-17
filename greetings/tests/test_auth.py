b9f1afb817af815821bf8155a79725087768cff7
apexdev <mwangi.em37@gmail.com>
2023-07-17 13:11:16 +0300

    test: Load client credentials
    Add tests to ensure that client credentials are loaded.
    Test should try load env variables from host machine or env file.

--- greetings/tests/test_auth.py
+++ greetings/tests/test_auth.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
+from unittest.mock import call, patch
 
-from unittest.mock import patch, MagicMock
-
-from greetings.views import encode_credentials
+from greetings.views import encode_credentials, load_env_credentials
 
 
 class AuthenticationTestCase(TestCase):
@@ -30,3 +29,60 @@ class AuthenticationTestCase(TestCase):
     self.assertIsInstance(actual, str)
     mock_b64_encode.assert_called_once_with(self.credential.encode('utf-8'))
     self.assertEqual(actual, expected)
+
+  @patch('greetings.views.os.environ')
+  def test_should_raise_exception_for_missing_credentials_on_both_host_and_env_file(self, mock_environ) -> None:
+    # Given
+    mock_environ.get.side_effect = [
+      None, None,   # Not found on host
+      None, None    # Not found in env file
+    ]    
+    
+    # Then
+    with self.assertRaises(ValueError):
+      load_env_credentials()  # When
+
+  @patch('greetings.views.os.environ')
+  @patch('greetings.views.environ.Env', autospec=True)
+  def test_should_prefer_load_client_credentials_from_host_machine(self, mock_env, mock_environ) -> None:
+    # Given
+    expected = {'CLIENT_ID': self.client_id, 'CLIENT_SECRET': self.client_secret}
+    mock_environ.get.side_effect = [
+      expected['CLIENT_ID'], expected['CLIENT_SECRET']    # Found on host
+    ]
+    
+    # When
+    actual = load_env_credentials()
+    
+    # Then    
+    mock_env.read_env.assert_not_called()
+    mock_environ.get.assert_has_calls([
+      call('CLIENT_ID', None),
+      call('CLIENT_SECRET', None)
+    ])
+    self.assertEqual(actual['CLIENT_ID'], expected['CLIENT_ID'])
+    self.assertEqual(actual['CLIENT_SECRET'], expected['CLIENT_SECRET'])
+
+  @patch('greetings.views.os.environ')
+  @patch('greetings.views.environ.Env', autospec=True)
+  def test_should_fallback_to_load_client_credentials_from_env_file(self,  mock_env, mock_environ) -> None:
+    # Given
+    expected = {'CLIENT_ID': self.client_id, 'CLIENT_SECRET': self.client_secret}
+    mock_environ.get.side_effect = [
+      None, None,                                         # Not found on host
+      expected['CLIENT_ID'], expected['CLIENT_SECRET']    # Found in env file
+    ]
+    
+    # When
+    actual = load_env_credentials()
+    
+    # Then    
+    mock_env.read_env.assert_called_once()
+    mock_environ.get.assert_has_calls([
+      call('CLIENT_ID', None), call('CLIENT_SECRET', None),   # Try load from host
+      call('CLIENT_ID', None),call('CLIENT_SECRET', None)     # Try load from env file
+    ])
+    self.assertEqual(actual['CLIENT_ID'], expected['CLIENT_ID'])
+    self.assertEqual(actual['CLIENT_SECRET'], expected['CLIENT_SECRET'])
+
+  
