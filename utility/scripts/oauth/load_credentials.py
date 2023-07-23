4a7edbfce9bfaae42d9424f545d4b3d063c8e799
apexdev <mwangi.em37@gmail.com>
2023-07-23 22:34:29 +0300

    refactor: Load credentials script
    Prefer logging over print statements for precise control over messages.
    Prefer environ package for more control over loading credentials.
    Extract functions to encapsulate logic in small expressive units.

--- utility/scripts/oauth/load_credentials.py
+++ utility/scripts/oauth/load_credentials.py
@@ -1,39 +1,52 @@
 import os
+import logging
 from pathlib import Path
 
 import environ
 
 
+DIR = Path(__file__).resolve().parent.parent.parent.parent
+CREDENTIALS_ENV: str = 'credentials.env'
+
+logger = logging.getLogger(__name__)
+env = environ.Env(
+  # set casting, default value
+  CLIENT_ID=(str, None),
+  CLIENT_SECRET=(str, None)
+)
+
 def main() -> None:
   
-  # Load dir
-  DIR = Path(__file__).resolve().parent.parent.parent.parent
-  print('path: ', DIR)
+  client_id, client_secret = load_from_host()
   
-  # Prefer load from host machine
-  client_id = os.environ.get('CLIENT_ID', None)
-  client_secret = os.environ.get('CLIENT_SECRET', None)
+  if not credentials_set(client_id, client_secret):
+    logger.warning('Credentials not found on host machine!')
+    client_id, client_secret = load_from_env_file()
   
-  if client_id is None and client_secret is None:
-    print('Credentials not found on host machine.')
-    
-    # Load from env file
-    print('Try \load credentials from env file...')
-    environ.Env.read_env(os.path.join(DIR, ".envs", "credentials.env"))
-    client_id = os.environ.get('CLIENT_ID', None)
-    client_secret = os.environ.get('CLIENT_SECRET', None)
-  
-    if client_id is None and client_secret is None:
-      print('Credentials not found on both host machine or env file',
-            'Set required env credentials on either your host machine or env file.')
+    if not credentials_set(client_id, client_secret):
+      logger.warning('Credentials not found in env file!')
+      logger.info('Configure required client credentials on host machine or env file.')
     else:
-      print('Credentials found in env file.')
-      print(f'client_id: {client_id} \nclient_secret: {client_secret}')
+      logger.info('Credentials loaded from env file.')
   
-  else:
-    print('Credentials found on host machine.')
-    print(f'client_id: {client_id} \nclient_secret: {client_secret}')
-    
+  logger.info(f'Credentials -> client_id: {client_id}, client secret: {client_secret}')     
+
+def credentials_set(id: str, secret: str) -> bool:
+  return id is not None and secret is not None
+
+def load_from_host() -> str:
+  logger.debug('Loading client credentials from host machine.')
+  return load_env_credentials()
+
+def load_from_env_file() -> str:
+  logger.debug('Loading client credentials from env file.')
+  environ.Env.read_env(os.path.join(DIR, ".envs", CREDENTIALS_ENV))
+  return load_env_credentials()
+  
+def load_env_credentials() -> str:
+  client_id = env('CLIENT_ID')
+  client_secret = env('CLIENT_SECRET')
+  return client_id, client_secret
 
 if __name__ == "__main__":
   main()
