2b98b612601349bba404d715bbcdbcdefb04ed0d
apexdev <mwangi.em37@gmail.com>
2023-07-25 13:07:48 +0300

    refactor: Extract oauth service
    Extract singleton auth service class to be responsible for auth tasks.
    Migrate OAuth logic in view to auth service  to encapsulate behavior.
    (chore): Ensure tests pass after refactor.
    
    Test:
      - Remove unnecessary imports.
      - Change the base module to auth service module.
      - Instantiate the auth service (under test) in class setUp functions.
      - Add test to ensure integration for recursive call is valid.

--- greetings/views.py
+++ greetings/views.py
@@ -1,10 +1,5 @@
-import base64
 import logging
-import os
 
-import environ
-import requests
-from django.core.exceptions import ImproperlyConfigured
 from django.core.handlers.wsgi import WSGIRequest
 from django.test import RequestFactory
 from oauth2_provider.decorators import protected_resource
@@ -13,19 +8,12 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from config.settings.base import BASE_DIR
 from greetings.models import Greeting
 from greetings.serializers import GreetingSerializer
-from greetings.utils.services import GreetingService
+from greetings.utils.services import GreetingService, OAuth2CredentialsService
 from greetings.utils.validators import GreetingPathValidator
 
 CUSTOM_GOODBYE: str = "Kwaheri"
-
-TOKEN_ENDPOINT: str = 'http://127.0.0.1:8000/o/token/'
-CONTENT_TYPE: str = "application/x-www-form-urlencoded"
-CACHE_CONTROL: str = "no-cache"
-AUTHORIZATION: str = "Basic {0}"
-GRANT_TYPE: str = "client_credentials"
 
 logger = logging.getLogger(__name__)
 factory = RequestFactory()
@@ -91,64 +79,12 @@ def custom_greeting_response(custom_greeting, request) -> Response:
 def try_make_recursive_call(initial_param: str, initial_request: Request) -> None:
     request = update_request_query_param(initial_param, initial_request)
 
-    # Logic to get token and make authorized request
-    credentials = load_env_credentials()
-    encoded_credential = encode_credentials(
-      credentials['CLIENT_ID'], credentials['CLIENT_SECRET'])
-    response = request_access_token(encoded_credential)
-    access_token = response.json()['access_token']
+    oauth_service = OAuth2CredentialsService()
+    access_token = oauth_service.get_access_token()
     request = authorize_request(access_token, request)
-
-    
+  
     logger.debug("recursive call to api_view: views.save_custom_greeting.")
     return make_recursive_call(request)
-
-
-def load_env_credentials() -> tuple[str]:
-  client_id, client_secret = load_envs()
-  
-  if not is_credentials_set(client_id, client_secret):
-    environ.Env.read_env(os.path.join(BASE_DIR, ".envs", "credentials.env"))
-    client_id, client_secret = load_envs()
-
-  if not is_credentials_set(client_id, client_secret):
-    raise ImproperlyConfigured('Client credentials not found on host machine or env file.')
-
-  return {'CLIENT_ID': client_id, 'CLIENT_SECRET': client_secret}
-
-def load_envs() -> str:
-  client_id = os.environ.get('CLIENT_ID', None)
-  client_secret = os.environ.get('CLIENT_SECRET', None)
-  return client_id, client_secret
-
-def is_credentials_set(id: str, secret: str) -> bool:
-  return id is not None and secret is not None
-
-def encode_credentials(client_id: str, client_secret: str) -> str:
-  credential = f'{client_id}:{client_secret}'
-  encoded_credential = base64.b64encode(credential.encode('utf-8'))
-  return encoded_credential.decode()
-
-def request_access_token(encoded_credential: str) -> Response:
-
-  response = requests.post(
-    url=TOKEN_ENDPOINT,
-    headers=get_headers(encoded_credential),
-    data=get_data(),
-    timeout=10
-  )
-  return response
-
-def get_headers(credential: str) -> dict[str, str]:
-  return {
-    # Add request headers here    
-    "Content-Type": CONTENT_TYPE,
-    "Cache-Control": CACHE_CONTROL,
-    "Authorization": AUTHORIZATION.format(credential),
-  }
-
-def get_data() -> dict[str, str]:
-  return {"grant_type": GRANT_TYPE}
 
 def authorize_request(token: str, request: WSGIRequest) -> WSGIRequest:
   auth = 'Bearer {0}'.format(token)
