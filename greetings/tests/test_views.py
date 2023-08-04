10e88fc89f1467b0fd27bf622cbc37e8cc9b1404
apexdev <mwangi.em37@gmail.com>
2023-08-04 22:44:54 +0300

    test: Add custom response testcase
    Add custom response TestCase to cover 'response.py' behavior.
    Use SimpleTestCase to directly test custom response logic and behavior.

--- greetings/tests/test_views.py
+++ greetings/tests/test_views.py
@@ -1,11 +1,15 @@
+import inspect
+
 from django.test import TestCase
 from django.utils import timezone
 from oauth2_provider.models import AccessToken
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APISimpleTestCase
 from rest_framework import status
 from rest_framework.response import Response
 
 from greetings.utils.constants import GreetingsPathConstants as path
+from greetings.utils.responses import CustomGreetingResponse, ErrorGreetingResponse
+
 
 BASE_MODULE = "greetings.views"
 
@@ -85,3 +89,78 @@ class RequestTestCase(TestCase):
             status_code=status.HTTP_201_CREATED,
             text="Saved custom greeting from user.",
         )
+
+class CustomResponseTestCase(APISimpleTestCase):
+  """
+  Test case to test custom wrapper response instances from DRF view.
+  """
+  
+  def setUp(self) -> None:
+    pass
+  
+  def test_should_return_rest_frameworks_response_instance_to_client(self) -> None:
+    # Given
+    expected = Response
+
+    # When
+    actual = CustomGreetingResponse(
+      status_code=200, message='test message', description='test description')
+    
+    # Then
+    self.assertIsInstance(actual, expected)
+
+  def test_should_omit_data_from_json_response_if_not_provided(self) -> None:
+    # Given
+    expected = 3
+
+    # When
+    actual = CustomGreetingResponse(
+      status_code=200, message='test message', description='test description',)
+    
+    # Then
+    self.assertEqual(len(actual.data), expected)
+
+  def test_should_raise_exception_if_required_arguments_are_missing(self) -> None:
+    # # Get the signature of the constructor
+    # constructor = CustomGreetingResponse.__init__
+    # signature = inspect.signature(constructor)
+
+    # # Count the number of parameters without default values
+    # required_args_count = sum(
+    #     1 for param in signature.parameters.values()
+    #     if param.default in [inspect.Parameter.empty, None]
+    # )
+
+    # Then
+    with self.assertRaisesMessage(
+      TypeError, 'required positional arguments'):
+      CustomGreetingResponse() # When
+    
+  def test_should_return_base_response_with_provided_actual_arguments(self) -> None:
+    # Given
+    expected = {
+      'status_code': 200, 
+      'message': 'test message', 
+      'description': 'test description', 
+      'some-key': 'some_value'}
+
+    actual = CustomGreetingResponse(
+      status_code=expected['status_code'], 
+      message=expected['message'], 
+      description=expected['description'], 
+      data={'some-key': 'some_value'})
+    
+    self.assertEqual(len(actual.data), len(expected))
+    self.assertDictEqual(actual.data, expected)
+
+  def test_should_return_error_default_values_on_non_provided_actual_arguments(self) -> None:
+    # Given
+    expected = {'message': 'Error', 
+                'description': 'Failed to save custom greeting submitted by user.'}
+
+    # When
+    actual = ErrorGreetingResponse()
+
+    self.assertEqual(actual.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertEqual(actual.data['message'], expected['message'])
+    self.assertEqual(actual.data['description'], expected['description'])
