b3f357796b222e8110af5b3bfda1ccc0ef850f8a
apexdev <mwangi.em37@gmail.com>
2023-08-07 13:03:16 +0300

    refactor: Clean service tests
    Clean and update tests to be more flexible and maintainable.
    
    - Prefer 'APIRequestFactory' for more control and function in tests.
    - Pass rest_framework 'Request'  to recursive service entry point.
    - Add test utility function to prepare stub initial request for tests.
    - Remove redundant and duplicate test.
    - Add constant for DRF view under mock.
    - Prefer get view url with 'reverse' function over constant enum value.

--- greetings/tests/test_services.py
+++ greetings/tests/test_services.py
@@ -2,13 +2,15 @@ import unittest
 from unittest.mock import patch
 
 from django.http import HttpRequest
-from django.test import RequestFactory
+from django.urls import reverse
+from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory
 
 from greetings.utils.constants import CUSTOM_GOODBYE
-from greetings.utils.constants import GreetingsPathConstants as path
 from greetings.utils.services import RecursiveViewService
 
 BASE_MODULE = "greetings.views"
+DRF_VIEW = 'save_custom_greeting'
 
 
 class RecursiveTestCase(unittest.TestCase):
@@ -18,27 +20,8 @@ class RecursiveTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.under_test = RecursiveViewService
-        factory = RequestFactory()
-        self.query_param = "greeting"
-        url = str(path.GREETING_URI) + self.query_param
-        self.initial_request = factory.post(path=url)
+        self.initial_request = prepare_initial_request()
 
-    def test_should_update_initial_request_query_param_value_with_custom_goodbye(
-        self,
-    ) -> None:
-        # Given
-        expected = CUSTOM_GOODBYE
-
-        # When
-        request = self.under_test._update_query_param(
-            self.query_param, self.initial_request
-        )
-        actual = request.META["QUERY_STRING"]
-
-        # Then
-        self.assertIsNotNone(actual)
-        self.assertIsInstance(actual, str)
-        self.assertIn(expected, actual)
 
     def test_should_log_debug_alert_message_when_making_recursive_call(self) -> None:
         # Given
@@ -47,90 +30,114 @@ class RecursiveTestCase(unittest.TestCase):
 
         # Then
         with self.assertLogs(service_logger, level="DEBUG") as cm:
-            self.under_test.make_recursive_call(
-                self.query_param, self.initial_request
-            )  # When
+            self.under_test.make_recursive_call(self.initial_request)  # When
             self.assertGreaterEqual(len(cm.output), 1)
             self.assertIn(expected, str(cm.output))
 
-    @patch(f"{BASE_MODULE}.save_custom_greeting")
+    @patch(f"{BASE_MODULE}.{DRF_VIEW}")
     def test_should_make_recursive_view_call_with_http_request_instance_argument(
         self, mock_view
     ) -> None:
         # Given
+        request = self.initial_request
         expected = HttpRequest
 
         # When
-        self.under_test._call_view(self.initial_request)
+        self.under_test.make_recursive_call(request)
         actual = mock_view.call_args[0][0]
 
         # Then
         mock_view.assert_called_once()
         self.assertIsInstance(actual, expected)
 
-    @patch(f"{BASE_MODULE}.save_custom_greeting")
+    @patch(f"{BASE_MODULE}.{DRF_VIEW}")
     def test_should_make_recursive_view_call_with_clone_of_initial_request(
         self, mock_view
     ) -> None:
         # Given
+        request = self.initial_request
         expected = {
-            "path": str(path.GREETING_ENDPOINT),
-            "method": "POST",
+            "path": request.path,
+            "method": request.method,
         }
 
         # When
-        self.under_test._call_view(self.initial_request)
+        self.under_test.make_recursive_call(request)
         actual = mock_view.call_args[0][0]
 
         # Then
         self.assertEqual(actual.path, expected["path"])
         self.assertEqual(actual.method, expected["method"])
 
-    @patch(f"{BASE_MODULE}.save_custom_greeting")
+    @patch(f"{BASE_MODULE}.{DRF_VIEW}")
     def test_should_make_recursive_view_call_with_goodbye_query_param_value(
         self, mock_view
     ) -> None:
         # Given
-        expected = CUSTOM_GOODBYE
+        request = self.initial_request
+        expected: dict = get_query_param(request, greeting=CUSTOM_GOODBYE)
 
         # When
-        self.under_test.make_recursive_call(self.query_param, self.initial_request)
+        self.under_test.make_recursive_call(request)
         request = mock_view.call_args[0][0]
-        actual = request.META["QUERY_STRING"].removeprefix("greeting=")
+        actual: str = request.META["QUERY_STRING"]
 
         # Then
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual.split('=')[0], expected['key'])
+        self.assertEqual(actual.split('=')[1], expected['value'])
 
-    @patch(f"{BASE_MODULE}.save_custom_greeting")
+    @patch(f"{BASE_MODULE}.{DRF_VIEW}")
     def test_should_make_recursive_view_call_with_authorized_request_argument(
         self, mock_view
     ) -> None:
         # Given
+        request = self.initial_request
         expected = "Authorization"
 
         # When
-        self.under_test._call_view(self.initial_request)
+        self.under_test.make_recursive_call(request)
         actual = mock_view.call_args[0][0]
 
         # Then
         mock_view.assert_called_once()
-        mock_view.assert_called_once_with(self.initial_request)
         self.assertTrue(actual.environ.get("HTTP_AUTHORIZATION"))
         self.assertIn(expected, actual.headers)
 
-    @patch(f"{BASE_MODULE}.save_custom_greeting")
+    @patch(f"{BASE_MODULE}.{DRF_VIEW}")
     def test_should_make_recursive_view_call_with_custom_greeting_data(self, mock_view) -> None:
       # Given
-      expected = {'key': 'greeting', 'value': self.query_param}
+      request = self.initial_request
+      expected: dict = get_query_param(request)
 
       # When
-      self.under_test.make_recursive_call(self.query_param, self.initial_request)
+      self.under_test.make_recursive_call(request)
       actual = mock_view.call_args[0][0]
 
       # Then
       mock_view.assert_called_once()
       self.assertTrue(actual.POST)
       self.assertIn(expected['key'], actual.POST.keys())
-      self.assertEqual(len(actual.POST), 1)
       self.assertIsInstance(actual.POST['greeting'], str)
       self.assertEqual(actual.POST['greeting'], expected['value'])
+
+
+# -------------------------------------------------------------------------------  
+# Test utility functions 
+# -------------------------------------------------------------------------------
+
+def prepare_initial_request() -> Request:
+  """ 
+  Create and mimic request passed to service from view.
+  NB: Should be instance of `rest_framework.request.Request`
+  """
+
+  factory = APIRequestFactory()  
+  url = reverse('greetings:save_custom_greeting')
+  request = factory.post(path=url, QUERY_STRING="greeting=tests", format='json')
+  return Request(request=request)
+
+def get_query_param(request: Request, greeting: str = None) -> dict:
+  query_param: str = request.META['QUERY_STRING']
+  key = query_param.split('=')[0]
+  value = query_param.split('=')[1] if greeting == None else greeting
+  return {'key': key, 'value': value}
