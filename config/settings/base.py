3d5fbf0e07a6839a2425ad5999c56a1c22d9d4c6
apexdev <mwangi.em37@gmail.com>
2023-08-12 12:38:35 +0300

    chore: Configure handler
    Configure project settings to define a custom DRF exception handler.

--- config/settings/base.py
+++ config/settings/base.py
@@ -172,7 +172,8 @@ REST_FRAMEWORK = {
   ),
   'DEFAULT_PERMISSION_CLASSES': (
     # 'rest_framework.permissions.IsAuthenticated',
-  )
+  ),
+  'EXCEPTION_HANDLER': 'greetings.utils.exceptions.custom_exception_handler',
 }
 
 OAUTH2_PROVIDER = {
